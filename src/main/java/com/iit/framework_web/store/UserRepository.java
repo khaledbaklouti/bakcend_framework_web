1063f6b1decd64d4dd56db108553d5439feacf2f
Khaled Baklouti <khaledbaklouti8@gmail.com>
2020-04-25 10:56:45 +0100

    add cors config

--- src/main/java/com/iit/framework_web/store/UserRepository.java
+++ src/main/java/com/iit/framework_web/store/UserRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-@RepositoryRestResource(path = "users", collectionResourceRel= "users")
+@RepositoryRestResource(path = "users", collectionResourceRel = "users")
 public interface UserRepository extends MongoRepository<User, String> {
     @RestResource(path = "username")
     User findByUsername(@Param("username") String username);
